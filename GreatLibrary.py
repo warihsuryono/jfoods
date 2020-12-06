31f399f30c3661c8b316cad048c3034f434cc8d4
warihsuryono <warih_suryono@yahoo.com>
2020-12-06 07:49:14 +0700

    aaa

--- GreatLibrary.py
+++ GreatLibrary.py
@@ -12,7 +12,7 @@ def add_rak(nama):
         return "Rak dengan nama " + nama + " berhasil ditambahkan"
 
 def add_buku(namaRak,namaBuku,pengarang,tahun,penerbit,genre):
-    if(namaBuku in dataBuku):
+    if(bukuAda(namaBuku)):
         return "Buku dengan nama [" + namaBuku + ", " + tahun + ", " + penerbit + ", " + genre + "] sudah ada di dalam sistem"
     elif(namaRak not in dataRak): 
         return "Rak dengan nama " + namaRak + " belum terdaftar di dalam sistem"
@@ -20,6 +20,40 @@ def add_buku(namaRak,namaBuku,pengarang,tahun,penerbit,genre):
         dataBuku.append([namaBuku,pengarang,tahun,penerbit,genre])
         dataLokasiBuku.append([namaBuku,namaRak])
         return "Buku dengan nama [" + namaBuku + ", " + tahun + ", " + penerbit + ", " + genre + "] berhasil ditambahkan"
+        
+def move_buku(namaBuku,namaRakBaru):
+    if(bukuAda(namaBuku) and namaRakBaru in dataRak):
+        countLokasiBuku = len(dataLokasiBuku)
+        try:
+            i = 0
+            while(i < countLokasiBuku):
+                if(namaBuku == dataLokasiBuku[i][0]):
+                    namaRakLama = dataLokasiBuku[i][1]
+                    dataLokasiBuku[i][1] = namaRakBaru
+                    return "Buku dengan nama [" + namaBuku + "] dipindahkan dari rak dengan nama [" + namaRakLama + "] ke rak dengan nama [" + namaRakBaru + "]"
+                i = i + 1
+        except Exception as e:
+            return "Pemindahan Buku ada kesalahan, silakan ulangi lagi!"
+        
+    else:
+        return "Buku dengan nama [" + namaBuku + "] dan/atau Rak [" + namaRakBaru + "] tidak ditemukan"
+    
+        
+def bukuAda(namaBuku):
+    countBuku = len(dataBuku)
+    if(countBuku <= 0):
+        return False
+    else:
+        try:
+            i = 0
+            while(i < countBuku):
+                if(namaBuku == dataBuku[i][0]):
+                    return True
+                i = i + 1
+            return False
+        except Exception as e:
+            return False
+            
 
 
 command = [""]
@@ -40,8 +74,19 @@ while command[0].upper() != "EXIT":
             errormessage = ""
             try:
                 print(add_buku(command[2],command[3],command[4],command[5],command[6],command[7]));
+            except Exception as e:
+                print(e)
+                errormessage = "Perintah ADD BUKU : ADD BUKU [Nama Rak] [Nama Buku] [Pengarang buku] [Tahun Terbit] [Penerbit] [Genre]"
+
+
+    if(command[0].upper() == "MOVE"):
+        if(command[1].upper() == "BUKU"):
+            errormessage = ""
+            try:
+                print(move_buku(command[2],command[3]));
             except Exception as e: 
-                errormessage = "Perintah ADD BUKU : ADD BUKU [Nama Rak] [Nama Buku] [Pengarang buku] [Tahun Terbit] [Penerbit] [Genre]!"
+                errormessage = "Perintah MOVE BUKU : MOVE BUKU [Nama buku] [Nama Rak baru]"
+        
                 
     elif(command[0] == "dataRak"):
         print(dataRak)
